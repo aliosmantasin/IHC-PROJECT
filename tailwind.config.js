38ea7730b09efea06ceab3321543ef4313931fe2
aliosmantasin <ali.osman-07@hotmail.com>
2023-11-08 14:58:02 +0300

    sa

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   theme: {
     extend: {
       screens:{
+        'xs': '425px',
+        // => @media (min-width: 425px) { ... }
+
         'sm': '640px',
       // => @media (min-width: 640px) { ... }
 
